6b77e1db70f0fef69c1eb26dbc9804b08c63b8de
Matthew Perry <perrym5@rpi.edu>
2011-12-14 14:20:32 -0500

    Adding some interactivity. Normalized the x-axis.

--- visualize.js
+++ visualize.js
@@ -32,6 +32,25 @@ google.setOnLoadCallback(function() {
         new google.visualization[type](this[0]).draw(data, options);
     };
 
+    // Extend Jquery for easy initializations
+    $.fn.init_viz = function(data, selected, sel_callback, change_callback, opts) {
+        // Populate line chart dropdown
+        select = $(this);
+        $.each(crime_fields, function(i, e) {
+            option = $('<option></option>').html(e.label).attr('value', e.col);
+            if(sel_callback(e, selected))
+                option.attr('selected', 'selected');
+            select.append(option);
+        });
+
+        // Wire change event
+        select.change(function() {
+            change_callback(data, $(this).find(':selected'), opts);
+        });
+
+        select.trigger('change');
+    };
+
     // Query logd for data
     /*
     var sparql_proxy = 'http://logd.tw.rpi.edu/sparql';
@@ -41,20 +60,34 @@ google.setOnLoadCallback(function() {
     };
     $.getJSON(sparql_proxy, params, function(data) {
         draw_chart(data, crime_fields);
-        draw_map(data, {label: 'Violent Crimes', col: 'violent_crime'});
+        draw_map(data, crime_fields[0]);
     });
     */
 
     // Use global data (saves development time)
-    draw_chart(data, crime_fields);
-    draw_map(data, crime_fields[0]);
+    $('#chart_select').init_viz(data, crime_fields,
+        function(e, sel) { return $.inArray(e, sel) != -1; },
+        function(data, selected, opts) {
+            draw_chart(data, selected.map(function(i, e) {
+                return { label: $(e).text(), col: $(e).val() }
+            }), opts);
+        }
+    );
+
+    $('#map_select').init_viz(data, crime_fields[0],
+        function(e, sel) { return e == sel; },
+        function(data, selected, opts) {
+            sel = $(selected[0]);
+            draw_map(data, { label: sel.text(), col: sel.val() }, opts);
+        }
+    );
 
     function draw_chart(data, fields, opts) {
         // Chart options
         var options = $.extend({
-            'title': 'Average Gross Income versus Number of Crimes',
+            'title': 'Adjusted Gross Income per Capita versus Number of Crimes',
             'hAxis': {
-                'title': 'Average Gross Income',
+                'title': 'Adjusted Gross Income per Capita',
                 'logScale': true
             },
             'vAxis': {
@@ -67,13 +100,18 @@ google.setOnLoadCallback(function() {
         }, opts);
 
         // Create data table
-        var cols = [['number', 'Average Gross Income']];
+        var cols = [['number', 'Adjusted Gross Income per Capita']];
         $.each(fields, function(i, f) { cols.push(['number', f.label]) });
 
         var rows = $.map(data.results.bindings, function(e) {
-            row = [parseInt(e.agi.value)];
+            row = [parseInt(e.agi.value) / parseInt(e.population.value)];
             $.each(fields, function(i, f) { row.push(parseInt(e[f.col].value)) });
             return [row];
+        });
+
+        // Sort rows appropriately
+        rows.sort(function(a, b) {
+            return ((a[0] < b[0]) ? -1 : ((a[0] > b[0]) ? 1 : 0));
         });
 
         // Display chart
