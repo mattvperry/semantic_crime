4fc7c501856ed067fb1a0b8f04952e9c48e9b843
Matthew Perry <perrym5@rpi.edu>
2011-12-13 19:24:06 -0500

    Modified the query to get state_abbrv without the semantic data.

--- visualize.js
+++ visualize.js
@@ -1,14 +1,30 @@
+// Team Chalupa
+// Web Science Fall 2011
+// Matt Perry and Josh Steiner
+// Semantic Government data visualized
+
 // Load google visualization packages
 google.load('visualization', '1', {'packages': ['geomap']});
 
-// Set google callback
+// Set Google callback
 google.setOnLoadCallback(function() {
-    var sparql_proxy = 'http://logd.tw.rpi.edu/sparql?';
-    var query_location = window.location.href + 'query.rq';
-    var params = $.param({'output': 'gvds', 'query-uri': query_location});
-    var query_url = sparql_proxy + params;
-    var query = new google.visualization.Query(query_url);
-    query.send(function(response) {
-        alert(response);
+    var sparql_proxy = 'http://logd.tw.rpi.edu/sparql';
+    var params = {
+        'output': 'json',
+        'query-uri': 'http://perrym5.github.com/semantic_crime/query.rq' //window.location.href + 'query.rq';
+    };
+    $.getJSON(sparql_proxy, params, function(data) {
+        draw_map(data, 'violent_crime');
     });
+
+    function draw_map(data, column) {
+        // Create google data table
+        var google_data = new google.visualization.DataTable();
+        google_data.addColumn('string', 'State');
+        google_data.addColumn('number', column);
+
+        // Populate google data table
+        $.each(data.results.bindings, function(i, e) {
+        });
+    };
 });
